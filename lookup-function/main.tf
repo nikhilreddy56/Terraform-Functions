ab0e49dc9e55cf714ae3462d8f990206251089db
Nikhil Ch <116032077+nikhilreddy56@users.noreply.github.com>
2025-11-10 22:40:08 -0500

    Update main.tf

--- lookup-function/main.tf
+++ lookup-function/main.tf
@@ -1,1 +1,26 @@
+#ADDING PROVIDERS
+terraform {
+    required_providers {
+    aws = {
+        source  = "hashicorp/aws"
+        version = "~> 5.0"
+    }
+    }
+}
 
+#GIVING ACCESS KEY AND SECRET-ACCESS KEY
+provider "aws" {
+    # region = "us-east-1"
+    access_key = "access-key"
+    secret_key = "secret-key"
+}
+
+#Creating Instances based on location and instance type 
+resource "aws_instance" "instance" {
+    ami                     = "ami-01bc990364452ab3e"
+    instance_type           = lookup(var.instance_type,var.location) 
+    #subnet_id               = aws_subnet.subnet1.id
+    tags = {
+    Name = "Terraform-instance"
+    }
+}
